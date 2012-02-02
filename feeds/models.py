f2de85fb04ab61bf3a770fd8a4f87284e2f0af36
Lisa van Gelder <lisa.van-gelder@guardian.co.uk>
2012-02-02 15:57:01 +0000

    First go at saving a story

--- feeds/models.py
+++ feeds/models.py
@@ -3,13 +3,13 @@ from django.db import models
 import datetime
 
 class Story(models.Model):
-    key = models.CharField(max_length = 255, db_index = True, unique = True)
+    key = models.CharField(max_length = 255, db_index = True)
     timestamp = models.DateTimeField(
         default = datetime.datetime.now, db_index = True
     )
     published = models.DateTimeField(null=True)
     title = models.CharField(blank=True, max_length=255, default="")
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     source = models.CharField(blank=True, max_length=255, default="")
     permalink = models.CharField(blank=True, max_length=255, default="")
     thumbnail = models.CharField(blank=True, max_length=255, default="")
