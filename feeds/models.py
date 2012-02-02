f8b5737b6598de7d6787bb3304d2f028982b8457
Lisa van Gelder <lisa.van-gelder@guardian.co.uk>
2012-02-02 14:44:48 +0000

    First test for saving a story

--- feeds/models.py
+++ feeds/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+import datetime
+
+class Story(models.Model):
+    key = models.CharField(max_length = 255, db_index = True, unique = True)
+    timestamp = models.DateTimeField(
+        default = datetime.datetime.now, db_index = True
+    )
+    published = models.DateTimeField(null=True)
+    title = models.CharField(blank=True, max_length=255, default="")
+    description = models.TextField()
+    source = models.CharField(blank=True, max_length=255, default="")
+    permalink = models.CharField(blank=True, max_length=255, default="")
+    thumbnail = models.CharField(blank=True, max_length=255, default="")
+    views = models.IntegerField(null=True)
+    searchterm = models.CharField(blank=True, max_length=255, default="")
--- feeds/tests.py
+++ feeds/tests.py
@@ -4,13 +4,19 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+import datetime
 
 from django.test import TestCase
+from feeds.models import Story
 
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
+    def test_store_story(self):
         """
         Tests that 1 + 1 always equals 2.
         """
-        self.assertEqual(1 + 1, 2)
+        story = Story.objects.create(title="foo", published = datetime.datetime(2012,02,02,12,12), )
+        story.save()
+
+        saved = Story.objects.get(title="foo")
+        self.assertEqual(saved.title, "foo")
