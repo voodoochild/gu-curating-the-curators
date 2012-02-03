5fb46f7f81b8f23f0916b60d49bb683ddf8bef69
Lisa van Gelder <lisa.van-gelder@guardian.co.uk>
2012-02-03 12:39:26 +0000

    Added tweetminster data

--- feeds/views.py
+++ feeds/views.py
@@ -21,3 +21,23 @@ def storify_dummy_data(request):
     }
     return HttpResponse(json.dumps(data),
                         content_type='application/json; charset=utf-8')
+
+
+def tweetminster_dummy_data(request):
+    storyjson = []
+    stories = Story.objects.filter(latest = True, source = 'Tweetminster')
+
+    for story in stories:
+        storyjson.append({
+            'key': story.key,
+            'title': story.title,
+            'thumbnail': story.thumbnail,
+            'permalink': story.permalink,
+        },)
+
+    random.shuffle(storyjson)
+    data = {
+        'stories': storyjson
+    }
+    return HttpResponse(json.dumps(data, encoding="utf-8"),
+                        content_type='application/json; charset=utf-8')
--- urls.py
+++ urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('',
     url(r'^$', 'app.views.dashboard', name='app_dashboard'),
     
     url(r'^storify-dummy-data/$', 'feeds.views.storify_dummy_data', name='feeds_storify_dummy_data'),
+    url(r'^tweetminster-dummy-data/$', 'feeds.views.tweetminster_dummy_data', name='feeds_storify_dummy_data'),
 )
