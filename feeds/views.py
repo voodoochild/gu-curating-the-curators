94cee3c42ae0027251c38e3677c5ce5c4be420d0
Lisa van Gelder <lisa.van-gelder@guardian.co.uk>
2012-02-03 11:55:46 +0000

    Changed the dummy data to be less dummy

--- feeds/views.py
+++ feeds/views.py
@@ -1,43 +1,23 @@
 import json
 import random
 from django.http import HttpResponse
+from feeds.models import Story
 
 def storify_dummy_data(request):
-    stories = [
-        {
-            'key': 'first_item',
-            'title': 'First item',
-            'thumbnail': 'http://p.twimg.com/AkmwZP8CQAI8vXL.jpg',
-            'permalink': 'http://storify.com/curious_scribe/egyptian-football-riots'
-        },
-        {
-            'key': 'second_item',
-            'title': 'Second item',
-            'thumbnail': 'http://p.twimg.com/AkmwZP8CQAI8vXL.jpg',
-            'permalink': 'http://storify.com/curious_scribe/egyptian-football-riots'
-        },
-        {
-            'key': 'third_item',
-            'title': 'Third item',
-            'thumbnail': 'http://p.twimg.com/AkmwZP8CQAI8vXL.jpg',
-            'permalink': 'http://storify.com/curious_scribe/egyptian-football-riots'
-        },
-        {
-            'key': 'fourth_item',
-            'title': 'Fourth item',
-            'thumbnail': 'http://p.twimg.com/AkmwZP8CQAI8vXL.jpg',
-            'permalink': 'http://storify.com/curious_scribe/egyptian-football-riots'
-        },
-        {
-            'key': 'fifth_item',
-            'title': 'Fifth item',
-            'thumbnail': 'http://p.twimg.com/AkmwZP8CQAI8vXL.jpg',
-            'permalink': 'http://storify.com/curious_scribe/egyptian-football-riots'
-        }
-    ]
-    random.shuffle(stories)
+    storyjson = []
+    stories = Story.objects.filter(latest = True)
+
+    for story in stories:
+        storyjson.append({
+            'key': story.key,
+            'title': story.title,
+            'thumbnail': story.thumbnail,
+            'permalink': story.permalink,
+        },)
+
+    random.shuffle(storyjson)
     data = {
-        'stories': stories
+        'stories': storyjson
     }
     return HttpResponse(json.dumps(data),
                         content_type='application/json; charset=utf-8')
---
+++ runfeed.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/lvgelder/r2/gu-curating-the-curators
+/home/lvgelder/.virtualenvs/curate/bin/python ./manage.py feed__storify
