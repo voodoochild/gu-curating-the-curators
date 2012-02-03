6f2682340a9441cd94eb7692fbf80b12147a0f0c
Kriss Watt <kriss.watt@gmail.com>
2012-02-03 13:54:30 +0000

    Updating the style to make it more Guardiany.

--- app/static/js/curators.js
+++ app/static/js/curators.js
@@ -33,17 +33,47 @@ Curators.Storify = (function() {
 })();
 
 
+Curators.Tweetminster = (function() {
+    var tmpl = '<li data-key="%key%">' +
+                '<a href="%permalink%"><h2>%title%</h2></a></li>';
+
+    /**
+     * Get latest Storify stories from the database.
+     */
+    var getData = function(callback) {
+        $.ajax({
+            url: '/tweetminster-dummy-data/',
+            success: callback
+        });
+    };
+
+    /**
+     * Render the supplied story to a string using the template.
+     */
+    var renderStory = function(story) {
+        return tmpl.replace('%key%', story['key'])
+                   .replace('%permalink%', story['permalink'])
+                   .replace('%title%', story['title']);
+    };
+
+    return {
+        'getData': getData,
+        'renderStory': renderStory
+    };
+})();
+
+
 Curators.Feeds = (function() {
     var subscribed_feeds = [];
-    var feed_timers = {};
-    var update_every = 60; // seconds
+    var update_every = 10; // seconds
+    var update_loop;
 
     /**
      * Return an array listing all possible feeds that can
      * be subscribed to. At the moment that's just Storify.
      */
     var availableFeeds = function() {
-        return ['Storify'];
+        return ['Storify','Tweetminster'];
     };
 
     /**
@@ -53,7 +83,6 @@ Curators.Feeds = (function() {
     var addFeed = function(feed) {
         if (-1 !== availableFeeds().indexOf(feed)) {
             subscribed_feeds.push(feed);
-            updateFeeds();
         }
         return false;
     };
@@ -68,28 +97,38 @@ Curators.Feeds = (function() {
             var $feed = $('[data-feed="' + feed + '"]');
             $feed.find('header').append(ajax_gif);
             
-            Curators[feed].getData(function(data) {
-                var $stories = $feed.find('ol li');
-                var new_stories = '';
+            setTimeout(function() {
+                Curators[feed].getData(function(data) {
+                    var $stories = $feed.find('ol li');
+                    var new_stories = '';
 
-                $.each(data['stories'], function(i, story) {
-                    new_stories += Curators[feed].renderStory(story);
+                    $.each(data['stories'], function(i, story) {
+                        new_stories += Curators[feed].renderStory(story);
+                    });
+
+                    var $new_stories = $(new_stories);
+                    $feed.find('ol').prepend($new_stories.hide());
+                    $stories.remove();
+                    $new_stories.show();
                 });
 
-                var $new_stories = $(new_stories);
-                $feed.find('ol').prepend($new_stories.hide());
-                $stories.remove();
-                $new_stories.show();
-            });
-            
-            $feed.find('header .ajax-loader').remove();
+                $feed.find('header .ajax-loader').remove();
+            }, 2000);
         });
+    };
+
+    /**
+     * Start a loop running to update the feeds at the required interval.
+     */
+    var startUpdates = function() {
+        clearInterval(update_loop);
+        update_loop = setInterval(updateFeeds, update_every * 1000);
     };
 
     return {
         'availableFeeds': availableFeeds,
         'addFeed': addFeed,
-        'updateFeeds': updateFeeds
+        'startUpdates': startUpdates
     };
 
 })();
@@ -97,4 +136,6 @@ Curators.Feeds = (function() {
 
 $(document).ready(function() {
     Curators.Feeds.addFeed('Storify');
+    Curators.Feeds.addFeed('Tweetminster');
+    Curators.Feeds.startUpdates();
 });
