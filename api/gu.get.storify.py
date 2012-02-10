19eb44ec0da7003a5629178bd4b9911159ed377e
Rev Dan Catt <revdancatt@gmail.com>
2012-02-10 16:19:44 +0000

    Make it so you can pass over the hashtag as a parameter

--- api/gu.get.storify.py
+++ api/gu.get.storify.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python
+import os
+import simplejson
+from admin.functions import get_query_string
 from admin.models import StorifySnapshots
 
+args = get_query_string(os.environ['QUERY_STRING'])
+
+if 'hashtag' in args:
+    hashtag = args['hashtag']
+else:
+    hashtag = 'default'
+
 #   get the record from the database
-snapshot = StorifySnapshots.gql("ORDER BY last_update DESC LIMIT 1")
+snapshot = StorifySnapshots.gql("WHERE hashtag = :1 ORDER BY last_update DESC LIMIT 1", hashtag)
+
+newResult = {}
+
+if snapshot.count() == 0:
+    newResult['stat'] = 'error'
+    newResult['error'] = '100: no records found with that hastag'
+else:
+    newResult['stat'] = 'ok'
+    newResult['result'] = simplejson.loads(snapshot[0].json)
 
 print 'Content-Type: application/json; charset=UTF-8'
 print ''
-print snapshot[0].json
+print simplejson.dumps(newResult)
